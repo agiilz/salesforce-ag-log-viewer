767139449927259ff8378cb3a040161be105f617
agiil <adriagil08@gmail.com>
2025-05-19 23:37:33 +0200

    traceFlag refactor + to do

--- src/traceFlag.ts
+++ src/traceFlag.ts
@@ -1,3 +1,14 @@
+/**
+    This module manages the creation and deletion of trace flags in Salesforce.
+    It ensures that a debug level exists in the org and creates a trace flag for the user if it doesn't exist.
+
+    TO DO:
+    - Add support for creation of custom debug levels with the parameters specified by the user
+    - Add support for enabling/disabling trace flags for different users
+    - Add support for trace flags for other entities (e.g. Apex classes, triggers)
+    - Add support for setting expiration date for trace flags
+*/
+
 import { Connection } from 'jsforce';
 import * as vscode from 'vscode';
 import { outputChannel } from './extension';
@@ -21,51 +32,36 @@ export interface TraceFlag {
     TracedEntityId: string;
 }
 
+//Define the debug level name/log type the same as the one used by the Developer Console
+const LEVEL_NAME = 'SFDC_DevConsole';
+const LOG_TYPE = 'DEVELOPER_LOG';
+
+//Function to ensure the trace flag is set for the user
+//This function will check if a trace flag already exists for the user and create one if it doesn't
 export async function ensureTraceFlag(connection: Connection, userId: string): Promise<void> {
     try {
         outputChannel.appendLine(`Checking trace flag for user: ${userId}`);
-        
+        //Set the debug level for the trace flag
         const debugLevelId = await ensureDebugLevel(connection);
-
+        
         // Check for existing trace flag
         const existingFlags = await connection.tooling.query<TraceFlag>(
-            `SELECT Id, DebugLevelId, LogType, StartDate, ExpirationDate FROM TraceFlag WHERE TracedEntityId = '${userId}' AND LogType = 'DEVELOPER_LOG'`
+            `SELECT Id, DebugLevelId, LogType, StartDate, ExpirationDate FROM TraceFlag WHERE TracedEntityId = '${userId}' AND LogType = '${LOG_TYPE}'`
         );
         outputChannel.appendLine(`Existing flags found: ${existingFlags.records?.length || 0}`);
-
-        const now = new Date();
-        const future = new Date(now);
-        future.setHours(future.getHours() + 24); // Set expiration to 24 hours from now
-
-        // Delete existing trace flags
+ 
+        //Delete existing trace flags from the user
         if (existingFlags.records && existingFlags.records.length > 0) {
             for (const flag of existingFlags.records) {
                 outputChannel.appendLine(`Deleting current trace flag: ${flag.Id}`);
+                //TO DO: Mass delete intead of looping through each flag to delete
                 await connection.tooling.delete('TraceFlag', flag.Id);
             }
         }
 
-        // Create new trace flag
-        outputChannel.appendLine('Creating new trace flag...');
-        const createResult = await connection.tooling.create('TraceFlag', {
-            TracedEntityId: userId,
-            DebugLevelId: debugLevelId,
-            LogType: 'DEVELOPER_LOG',
-            StartDate: now.toISOString(),
-            ExpirationDate: future.toISOString()
-        });
-
-        if (Array.isArray(createResult)) {
-            if (!createResult[0].success) {
-                throw new Error(`Failed to create trace flag: ${JSON.stringify(createResult[0].errors)}`);
-            }
-            outputChannel.appendLine(`Created new trace flag with ID: ${createResult[0].id}`);
-        } else {
-            if (!createResult.success) {
-                throw new Error(`Failed to create trace flag: ${JSON.stringify(createResult.errors)}`);
-            }
-            outputChannel.appendLine(`Created new trace flag with ID: ${createResult.id}`);
-        }
+    //Create new trace flag
+    const traceFlagId = await createTraceFlag(connection, userId, debugLevelId);
+    outputChannel.appendLine(`Successfully created and activated trace flag: ${traceFlagId}`);
 
     } catch (error: any) {
         const errorMessage = error?.message || 'Unknown error occurred';
@@ -75,13 +71,36 @@ export async function ensureTraceFlag(connection: Connection, userId: string): P
     }
 }
 
+//Function to create a new trace flag for the user
+async function createTraceFlag(connection: Connection, userId: string, debugLevelId: string): Promise<string> {
+    const now = new Date();
+    const future = new Date(now);
+    future.setHours(future.getHours() + 24); // Set expiration to 24 hours from now
+
+    // Create new trace flag
+    const result = await connection.tooling.create('TraceFlag', {
+        TracedEntityId: userId,
+        DebugLevelId: debugLevelId,
+        LogType: LOG_TYPE,
+        StartDate: now.toISOString(),
+        ExpirationDate: future.toISOString()
+    });
+
+    const response = Array.isArray(result) ? result[0] : result;
+
+    if (!response.success) {
+        throw new Error(`Failed to create new trace flag: ${JSON.stringify(response.errors)}`);
+    }
+    outputChannel.appendLine(`Created new trace flag with ID: ${response.id}`);
+    return response.id;
+}
+
+//Function to ensure the debug level exists in the org
 async function ensureDebugLevel(connection: Connection): Promise<string> {
-    const LEVEL_NAME = 'SFDC_DevConsole';
-    
     try {
         outputChannel.appendLine(`Checking for debug level: ${LEVEL_NAME}`);
         
-        // Check for existing debug level
+        // Check for existing debug level in the org
         const existingLevels = await connection.tooling.query<DebugLevel>(
             `SELECT Id, ApexCode, Visualforce, Database, System FROM DebugLevel WHERE DeveloperName = '${LEVEL_NAME}'`
         );
@@ -89,36 +108,38 @@ async function ensureDebugLevel(connection: Connection): Promise<string> {
         if (existingLevels.records && existingLevels.records.length > 0) {
             outputChannel.appendLine(`Found existing debug level: ${existingLevels.records[0].Id}`);
             return existingLevels.records[0].Id;
+        }else{
+            // Create new debug level if there are none
+            outputChannel.appendLine('No existing debug level found, creating a new one...');
+            return await createDebugLevel(connection);
         }
-
-        // Create new debug level if there are none
-        outputChannel.appendLine('No existing debug level found, creating a new one...');
-        const result = await connection.tooling.create('DebugLevel', {
-            DeveloperName: LEVEL_NAME,
-            MasterLabel: 'SF Log Viewer Debug Level',
-            ApexCode: 'FINE',
-            Visualforce: 'INFO',
-            Database: 'INFO',
-            System: 'DEBUG'
-        });
-
-        if (Array.isArray(result)) {
-            if (!result[0].success) {
-                throw new Error(`Failed to create debug level: ${JSON.stringify(result[0].errors)}`);
-            }
-            outputChannel.appendLine(`Created new debug level with ID: ${result[0].id}`);
-            return result[0].id;
-        }
-        
-        if (!result.success) {
-            throw new Error(`Failed to create debug level: ${JSON.stringify(result.errors)}`);
-        }
-        outputChannel.appendLine(`Created new debug level with ID: ${result.id}`);
-        return result.id;
+            
     } catch (error: any) {
         const errorMessage = error?.message || 'Unknown error occurred';
         outputChannel.appendLine(`Debug level error: ${error}`);
         vscode.window.showErrorMessage(`Failed to manage debug level: ${errorMessage}`);
         throw error;
     }
+}
+
+//Function to create a new debug level in the org
+async function createDebugLevel(connection: Connection): Promise<string> {
+
+    const result = await connection.tooling.create('DebugLevel', {
+        DeveloperName: LEVEL_NAME,
+        MasterLabel: 'SF Log Viewer Debug Level',
+        ApexCode: 'FINE',
+        Visualforce: 'INFO',
+        Database: 'INFO',
+        System: 'DEBUG'
+    });
+
+    const response = Array.isArray(result) ? result[0] : result; 
+    
+    if (!response.success) {
+        throw new Error(`Failed to create debug level: ${JSON.stringify(response.errors)}`);
+    }
+
+    outputChannel.appendLine(`Created new debug level with ID: ${response.id}`);
+    return response.id;
 }
